bb103e3fe63071fa9bd554bf3c38e3327b76e466
mnagi5370-boop <mnagi5370@gmail.com>
2025-12-12 14:02:09 +0200

    client.lua
    Added phone functionality with SMS and call handling.

--- client.lua
+++ client.lua
@@ -1,6 +1,8 @@
+-- client.lua
 local phoneBrowser = nil
 local phoneVisible = false
 
+-- وظيفة لفتح وإغلاق التليفون
 function togglePhone()
     if not phoneBrowser then
         phoneBrowser = guiCreateBrowser(0, 0, 1, 1, true, true, false)
@@ -16,4 +18,28 @@ function togglePhone()
     showCursor(phoneVisible)
 end
 
-bindKey("M", "down", togglePhone) -- M تفتح التليفون
+bindKey("m", "down", togglePhone) -- M تفتح التليفون
+
+-- استقبال أحداث من السيرفر (Calls, SMS, Camera, SIM, Settings, Store)
+addEvent("phone:receiveSMS", true)
+addEventHandler("phone:receiveSMS", root, function(fromNumber, message)
+    if phoneBrowser then
+        executeBrowserJavascript(phoneBrowser, ("addMessage('%s', '%s', 'in');"):format(fromNumber, message))
+    end
+end)
+
+addEvent("phone:receiveCall", true)
+addEventHandler("phone:receiveCall", root, function(fromNumber)
+    if phoneBrowser then
+        executeBrowserJavascript(phoneBrowser, ("incomingCall('%s');"):format(fromNumber))
+    end
+end)
+
+-- مثال لإرسال حدث للسيرفر من HTML
+function sendSMSToServer(toNumber, message)
+    triggerServerEvent("phone:sendSMS", resourceRoot, toNumber, message)
+end
+
+function makeCall(toNumber)
+    triggerServerEvent("phone:makeCall", resourceRoot, toNumber)
+end
